844b3fdb13fbb4b4adf8d84c69e8796562bfb9a3
yoogukhyeon <rnrgus5893@gmail.com>
2021-05-04 00:03:06 -1200

    2021 05/04 인하대병원 프로젝트

---
+++ js/main.js
@@ -0,0 +1,55 @@
+$(document).ready(function(){
+
+    var swiper = new Swiper('.swiper-container', {
+      spaceBetween: 30,
+      centeredSlides: true,
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: true,
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
+    /*slide-button*/
+      $(".swiper-button-pause").click(function(){
+                swiper.autoplay.stop();
+                $('.swiper-button-play').stop().show();
+                $(".swiper-button-pause").stop().hide();
+            });
+
+            $(".swiper-button-play").click(function(){
+                swiper.autoplay.start();
+                $(".swiper-button-pause").stop().show();
+                 $('.swiper-button-play').stop().hide();
+             
+            }); 
+ 
+    
+    /*new slider*/
+     var swiper = new Swiper('.swiper-container2', {
+      pagination: {
+        el: '.swiper-pagination',
+      },
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: true,
+      }
+    });
+
+    
+
+    
+
+  
+    
+ 
+    
+    
+})
+    
