c3757c4a0c3369a0328b1b9b46fb9dbae022ab63
yoogukhyeon <rnrgus5893@gmail.com>
2021-05-05 16:58:50 -1200

    2021 05 / 05 인하대학교 모바일 html 코드 정리

--- js/main.js
+++ js/main.js
@@ -1,58 +1,47 @@
-$(document).ready(function(){
-
-        
-        
+$(document).ready(function () {
     var sswiper = new Swiper('.swiper-container', {
-      spaceBetween: 30,
-      centeredSlides: true,
-      autoplay: {
-        delay: 2500,
-        disableOnInteraction: true,
-      },
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-      },
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-        
-    });
-      /*slide-button*/
-      $(".swiper-button-pause").click(function(){
-                sswiper.autoplay.stop();
-                $(".swiper-button-play").stop().show();
-                $(".swiper-button-pause").stop().hide();
-            });
+        spaceBetween: 30,
+        centeredSlides: true,
+        autoplay: {
+            delay: 2500,
+            disableOnInteraction: true,
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
 
-            $(".swiper-button-play").click(function(){
-                sswiper.autoplay.start();
-                $(".swiper-button-pause").stop().show();
-                 $(".swiper-button-play").stop().hide();
-             
-            }); 
- 
-    
-    /*new slider*/
-     var swiper = new Swiper('.swiper-container2', {
-      pagination: {
-        el: '.swiper-pagination',
-      },
-      autoplay: {
-        delay: 2500,
-        disableOnInteraction: true,
-      }
+    });
+    /*slide-play-button*/
+    $(".swiper-button-pause").click(function () {
+        sswiper.autoplay.stop();
+        $(".swiper-button-play").stop().show();
+        $(".swiper-button-pause").stop().hide();
     });
 
-    
+    $(".swiper-button-play").click(function () {
+        sswiper.autoplay.start();
+        $(".swiper-button-pause").stop().show();
+        $(".swiper-button-play").stop().hide();
 
-    
+    });
 
-  
-    
- 
-    
-    
+    /*news slider*/
+    var swiper = new Swiper('.swiper-container2', {
+        pagination: {
+            el: '.swiper-pagination',
+        },
+        autoplay: {
+            delay: 2500,
+            disableOnInteraction: true,
+        }
+    });
+
+
+
+
 })
-    
